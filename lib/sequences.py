5e0089861508236144f46dea62e0242c1742424b
Irene Mutegi <irene@Ms-Echo.>
2024-11-27 22:27:23 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    
+    fibonacci = [0, 1]  
+
+   
+    for i in range(2, length):
+        next_number = fibonacci[i - 1] + fibonacci[i - 2]
+        fibonacci.append(next_number)
+
+
+    print(fibonacci[:length])
+
+
+print_fibonacci(9)  
